773f7c07c543942a154b89d5f2b88cc11c4f46b5
steve0503 <leehr76@gmail.com>
2014-01-22 11:28:59 +0900

    OneToFifty

--- OneToFifty/ViewController.m
+++ OneToFifty/ViewController.m
@@ -8,15 +8,60 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define MAX_NUM 25
+
+
+@interface ViewController (){
+
+    NSMutableArray *RandomNumbers;
+ 
+}
 
 @end
 
 @implementation ViewController
 
+
+-(NSMutableArray *)getRandomNumbers {
+    
+        NSMutableArray *listOfNumbers = [[NSMutableArray alloc] init];
+        for (int i = 0 ; i < MAX_NUM ; ++i) {
+            [listOfNumbers addObject:[NSNumber numberWithInt:i]];
+        }
+        NSMutableArray *uniqueNumbers = [[NSMutableArray alloc] init];
+        int r;
+        while ([uniqueNumbers count] < 25) {
+            r = arc4random() % [listOfNumbers count];
+            if (![uniqueNumbers containsObject:[listOfNumbers objectAtIndex:r]]) {
+                [uniqueNumbers addObject:[listOfNumbers objectAtIndex:r]];
+            }
+        }
+    
+    
+    NSUInteger i = 0;
+    
+    for (i = 0; i < 25 ; i++) {
+        
+        NSLog(@"RandomNumber:%d ",(NSUInteger)[[uniqueNumbers objectAtIndex:i] integerValue]);
+        
+    }
+    
+    
+    return uniqueNumbers;
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    RandomNumbers =  [self getRandomNumbers];
+    
+
+   
+
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
