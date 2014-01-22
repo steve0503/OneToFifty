ddc1834072ea7f7fc30460b3199ef0df38eefca6
steve0503 <leehr76@gmail.com>
2014-01-22 10:19:30 +0900

    Initial Commit

---
+++ OneToFifty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OneToFifty
+//
+//  Created by SDT-1 on 2014. 1. 22..
+//  Copyright (c) 2014년 Maybe There. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OneToFifty/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OneToFifty
+//
+//  Created by SDT-1 on 2014. 1. 22..
+//  Copyright (c) 2014년 Maybe There. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
