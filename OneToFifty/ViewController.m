c36e5e35ffd93ffe9a18b62897dbee813e9ae4cf
steve0503 <leehr76@gmail.com>
2014-01-22 12:35:05 +0900

    Timer updated

--- OneToFifty/ViewController.m
+++ OneToFifty/ViewController.m
@@ -14,12 +14,59 @@
 @interface ViewController (){
 
     NSMutableArray *RandomNumbers;
- 
+
 }
+
+
+@property (strong, nonatomic) NSTimer *stopWatchTimer; // Store the timer that fires after a certain time
+@property (strong, nonatomic) NSDate *startDate; // Stores the date of the click on the start button
+
+
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+
+- (IBAction)onStartPressed:(id)sender;
+- (IBAction)onStopPressed:(id)sender;
 
 @end
 
 @implementation ViewController
+
+
+- (IBAction)onStopPressed:(id)sender {
+    [self.stopWatchTimer invalidate];
+    self.stopWatchTimer = nil;
+    [self updateTimer];
+}
+
+
+- (IBAction)onStartPressed:(id)sender {
+    self.startDate = [NSDate date];
+    
+    // Create the stop watch timer that fires every 100 ms
+    self.stopWatchTimer = [NSTimer scheduledTimerWithTimeInterval:1.0/10.0
+                                                           target:self
+                                                         selector:@selector(updateTimer)
+                                                         userInfo:nil
+                                                          repeats:YES];
+}
+
+- (void)updateTimer
+{
+    // Create date from the elapsed time
+    NSDate *currentDate = [NSDate date];
+    NSTimeInterval timeInterval = [currentDate timeIntervalSinceDate:self.startDate];
+    NSDate *timerDate = [NSDate dateWithTimeIntervalSince1970:timeInterval];
+    
+    // Create a date formatter
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"HH:mm:ss.SSS"];
+    [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
+    
+    // Format the elapsed time and set it to the label
+    NSString *timeString = [dateFormatter stringFromDate:timerDate];
+    self.timerLabel.text = timeString;
+}
+
 
 
 -(NSMutableArray *)getRandomNumbers {
@@ -56,10 +103,6 @@
     [super viewDidLoad];
     
     RandomNumbers =  [self getRandomNumbers];
-    
-
-   
-
     
     
 	// Do any additional setup after loading the view, typically from a nib.
